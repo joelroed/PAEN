fd011275572e7bbb3e755e6d68efb33a312eed3f
Joel Roed <jooel.roed@gmail.com>
2022-06-11 16:56:01 +0200

    Final Commit w/out output

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const number = prompt("Enter number: ")
+// const number = prompt("Enter number: ")
 
-if(number % 2 == 0) {
-    console.log("The number is even");
+// if(number % 2 == 0) {
+//     console.log("The number is even");
+// }
+// else {
+//     console.log("The number is odd")
+// }
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = numbers.filter(checkEven);
+
+function checkEven(number) {
+    if(number % 2 === 0)
+    console.log(number);
 }
-else {
-    console.log("The number is odd")
-}
+
+
+
